349a25ef3d68ca86de1deccab8d1cf4be9c4782d
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2024-07-02 18:02:38 -0400

    Day 16 ✅

---
+++ 100 Days of SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nick Richardson on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName: "fan")
+        Text("Hello, world")
+    }
+}
+
+#Preview {
+    ContentView()
+}
